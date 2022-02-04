605b30905e3e16951830e19f318e8d196d3990ba
Julian Ruiz <Jj.ruiz.23@hotmail.com>
2022-02-04 15:37:33 -0500

    Comentariado de codigo

--- delete.py
+++ delete.py
@@ -4,11 +4,11 @@ from pymongo import mongo_client    # libreria para manejo de mongo mediante pyt
 client = mongo_client('localhost:27017')    # creo la conexion
 db = client.EmployeeData    # creo la base pasando  los datos  conexion.nombre de nueva base
 
-def delete():
-
-    try:
-        criterio = input('\N Enter employed id to delete \n')
-        db.Employees.delete_many({'id': criterio})
+def delete():   # funcion
+    # manejo de errores o excepciones
+    try:    # realice
+        criterio = input('\N Enter employed id to delete \n')   # captura de dato para borrar
+        db.Employees.delete_many({'id': criterio})  # eliminar registro  base.documento.modulo(registro seleccionado)
         print('\n Deletion Successful \n')
     except ImportError:
         platform_specific_module = None
