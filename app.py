f504c4e99871863f8aac85b4aca62a12e1d15b94
Julian Ruiz <Jj.ruiz.23@hotmail.com>
2022-02-04 12:29:11 -0500

    Primer modificacion al codigo

--- app.py
+++ app.py
@@ -4,25 +4,20 @@ import update
 import delete
 
 def run():
-    persona1 = Programa()
-    persona1.menu()
 
-class Programa:
-    def __init__(self):
-        self.dato = 1
-    def menu(self):
-
-        mensaje = '''SELECCIONE UNA OPCION
-        1) INSERT
-        2) UPDATE
-        3) READ
-        4) DELETE'''
-
-        while self.dato:
-
-             opcion = int(input(mensaje))
+    opcion = 
+    menu()
 
 
+def menu():
+    mensaje = '''SELECCIONE UNA OPCION
+    1) INSERT
+    2) UPDATE
+    3) READ
+    4) DELETE
+    '''
+
+    opcion = int(input(mensaje))
 
 
 if __name__ == '__main__':
