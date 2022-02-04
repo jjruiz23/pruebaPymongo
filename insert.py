8e1844534fa97bf717217e32bb5026b933a22a97
Julian Ruiz <Jj.ruiz.23@hotmail.com>
2022-02-04 13:57:15 -0500

    funcion insert para capturar datos y enviarlos por json a  pymongo y uso de excepcion

--- insert.py
+++ insert.py
@@ -1,5 +1,25 @@
+from platform import platform
 from pymongo import mongo_client
 
 client = mongo_client('localhost:27017')
 db = client.EmployeeData
 
+def insert():
+    try:
+        employee_id = input('Enter employed id: ')
+        employee_name = input('Enter name: ')
+        employee_country = input('Enter country: ')
+        employee_age = input('Enter age: ')
+
+        db.Employees.insert_one(
+        {
+            'id': employee_id,
+            'name': employee_name,
+            'couentry': employee_country,
+            'age': employee_age
+
+        })
+        print('Inserted data successfully')
+
+    except ImportError:
+        platform_specific_module = None
