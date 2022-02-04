df2363d23ff4cf26d1e037850654e96d1719ae4f
Julian Ruiz <Jj.ruiz.23@hotmail.com>
2022-02-04 14:23:44 -0500

    Adicion de comentarios al codigo ready.py

--- read.py
+++ read.py
@@ -4,12 +4,13 @@ from pymongo import mongo_client    # libreria para manejo de mongo mediante pyt
 client = mongo_client('localhost:27017')    # creo la conexion
 db = client.EmployeeData    # creo la base pasando  los datos  conexion.nombre de nueva base
 
-def read():
-    try:
-        emp_lista = db.Employees.find()
-        print('\n All data from EmployeeData DataBase \n')
-        for emp in emp_lista:
-            print(emp)
+def read(): # funcion
+    #menejo de exceciones error
+    try:    # realice
+        emp_lista = db.Employees.find() # buscar datos de documento   base.documento.buscar()
+        print('\n All data from EmployeeData DataBase \n')  # mensaje
+        for emp in emp_lista:   # recorrer los datos almacenados en la busqueda
+            print(emp)  # impimir dato en cada paso del recorrido
 
-    except ImportError:
+    except ImportError: # manejo de errores
         platform_specific_module = None
